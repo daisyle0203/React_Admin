06cc67e1e989520ac1281318d47d64bdf7c3ba9f
Daisy Le <daisy.le0203@gmail.com>
2022-12-21 12:21:47 -0600

    setup the app with color mode context

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,5 @@
 import { createContext, useState, useMemo } from "react"
 import { createTheme } from "@mui/material/styles"
-import { co } from "@fullcalendar/core/internal-common"
 
 // color design tokens
 export const tokens = (mode) => ({
